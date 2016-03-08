4a5808acb36116d1c144587231473e1dcad7cee7
Emma <emma@moirai.cn>
2016-03-08 10:37:30 +0800

    update bus

--- src/com/dhcc/model/Bus.java
+++ src/com/dhcc/model/Bus.java
@@ -1,5 +1,96 @@
 package com.dhcc.model;
 
 public class Bus {
+	private int fbus;
+	private int tbus;
+	private double r;
+	private double x;
+	private double b;
+	private double rateA;
+	private double rateB;
+	private double rateC;
+	private double ratio;
+	private double angle;
+	private double status;
+	private double angmin;
+	private double angmax;
+	public int getFbus() {
+		return fbus;
+	}
+	public void setFbus(int fbus) {
+		this.fbus = fbus;
+	}
+	public int getTbus() {
+		return tbus;
+	}
+	public void setTbus(int tbus) {
+		this.tbus = tbus;
+	}
+	public double getR() {
+		return r;
+	}
+	public void setR(double r) {
+		this.r = r;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getB() {
+		return b;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	public double getRateA() {
+		return rateA;
+	}
+	public void setRateA(double rateA) {
+		this.rateA = rateA;
+	}
+	public double getRateB() {
+		return rateB;
+	}
+	public void setRateB(double rateB) {
+		this.rateB = rateB;
+	}
+	public double getRateC() {
+		return rateC;
+	}
+	public void setRateC(double rateC) {
+		this.rateC = rateC;
+	}
+	public double getRatio() {
+		return ratio;
+	}
+	public void setRatio(double ratio) {
+		this.ratio = ratio;
+	}
+	public double getAngle() {
+		return angle;
+	}
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+	public double getStatus() {
+		return status;
+	}
+	public void setStatus(double status) {
+		this.status = status;
+	}
+	public double getAngmin() {
+		return angmin;
+	}
+	public void setAngmin(double angmin) {
+		this.angmin = angmin;
+	}
+	public double getAngmax() {
+		return angmax;
+	}
+	public void setAngmax(double angmax) {
+		this.angmax = angmax;
+	}
 
 }
